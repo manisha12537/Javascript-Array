e67318d99a9bf8013db3b134f5ff6e2efd3561bf
manisha12537 <89977220+manisha12537@users.noreply.github.com>
2022-02-15 15:10:18 +0530

    Add files via upload

---
+++ logocalQ.js
@@ -0,0 +1,15 @@
+var list=[8,6,2,3,8,7,1,5]
+var list2=[]
+if(list.length%2==0){
+   var a=list.length/2
+   var b=a
+   while(b<list.length){
+       list2.push(list[b])
+       b++
+}
+var i=0
+while(i<a){
+    list2.push(list[i])
+    i++
+}}
+console.log(list2)
